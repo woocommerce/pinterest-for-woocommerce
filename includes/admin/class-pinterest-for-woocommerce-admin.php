9e44de726a7bbe48b662894ca436990619f5a0cd
Kostas Seresiotis <kostas@saucal.com>
2021-04-13 22:10:16 +0300

    cleanup unneeded files

--- includes/admin/class-pinterest-for-woocommerce-admin.php
+++ includes/admin/class-pinterest-for-woocommerce-admin.php
@@ -30,7 +30,6 @@ class Pinterest_For_Woocommerce_Admin {
 	 * Include any classes we need within admin.
 	 */
 	public function includes() {
-		include_once 'pinterest-for-woocommerce-admin-functions.php';
 		include_once 'class-pinterest-for-woocommerce-admin-assets.php';
 		include_once 'class-pinterest-for-woocommerce-admin-settings-page.php';
 	}
