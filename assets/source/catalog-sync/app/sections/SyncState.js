6d9a00cd150655d72ef0d907f6ba3c36cfddb091
Paulo Iankoski <me@wpdev.com.br>
2021-05-31 22:13:24 -0300

    Add State section with hardcoded content

---
+++ assets/source/catalog-sync/app/sections/SyncState.js
@@ -0,0 +1,51 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	Button,
+	Card,
+	CardHeader,
+	CardBody,
+	CardFooter,
+	CheckboxControl,
+	Icon,
+	__experimentalText as Text,
+} from '@wordpress/components';
+
+/**
+ * Internal dependencies
+ */
+import Emoji from '../components/Emoji';
+
+const SyncState = () => {
+	const StateItem = () => {
+		return (
+			<div className="woocommerce-catalog-sync__state-item">
+				<Text variant="muted" className="muted"><Emoji symbol="✅" label="checked" /> 31 March 2021, 07:12am</Text>
+				<Text variant="subtitle">Feed pulled by Pinterest</Text>
+				<Text variant="muted" className="muted">This feed contained 315 products.</Text>
+			</div>
+		);
+	}
+
+	return (
+		<Card className="woocommerce-table">
+			<CardHeader>
+				<Text variant="title.small" as="h2">
+					{ __( 'Latest Activity' ) }
+				</Text>
+			</CardHeader>
+			<CardBody className="woocommerce-catalog-sync__state-body">
+				<StateItem />
+				<StateItem />
+				<StateItem />
+			</CardBody>
+			<CardFooter className="align-items-start">
+				<Button isLink>Load more</Button>
+			</CardFooter>
+		</Card>
+	)
+};
+
+export default SyncState;
