9e602501ed185deb8a3faad2522dc42fd98f45d4
Paulo Iankoski <me@wpdev.com.br>
2021-06-02 14:48:02 -0300

    Connect SyncIssues component to store data

--- assets/source/catalog-sync/app/sections/SyncIssues.js
+++ assets/source/catalog-sync/app/sections/SyncIssues.js
@@ -2,18 +2,19 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { compose } from '@wordpress/compose';
+import { withSelect } from '@wordpress/data';
 import {
-	Button,
-	Card,
-	CardHeader,
-	CardBody,
-	CheckboxControl,
 	Icon,
-	__experimentalText as Text,
 } from '@wordpress/components';
 import { TableCard } from '@woocommerce/components';
 
-const SyncIssues = () => {
+/**
+ * Internal dependencies
+ */
+import { REPORTS_STORE_NAME } from '../data';
+
+const SyncIssues = ({ feedIssues }) => {
 	const defaultHeaderAttributes = {
 		isLeftAligned: true,
 		isSortable: false,
@@ -26,51 +27,52 @@ const SyncIssues = () => {
 		{ key: 'edit', ...defaultHeaderAttributes },
 	];
 
-	const rows = [
-		[
-			{ display: <Icon icon="warning" className="red-text" /> },
-			{ display: 'Pink marble tee' },
-			{ display: 'Description is missing from product metadata.' },
-			{ display: <a href="#">Edit</a> },
-		],
-		[
-			{ display: <Icon icon="warning" className="yellow-text" /> },
-			{ display: 'Yellow slippers #3' },
-			{ display: 'The condition of the item is missing. The column is optional but we encourage users to specify...' },
-			{ display: <a href="#">Edit</a> },
-		],
-		[
-			{ display: <Icon icon="warning" className="yellow-text" /> },
-			{ display: 'Yellow slippers #3' },
-			{ display: 'google_product_category is invalid. The column is optional but we encourage users to follow...' },
-			{ display: <a href="#">Edit</a> },
-		],
-		[
-			{ display: <Icon icon="warning" className="red-text" /> },
-			{ display: 'Blue Bob Cake' },
-			{ display: 'Your product link doesn\'t match the verified website associated with your account. Make sure...' },
-			{ display: <a href="#">Edit</a> },
-		],
-		[
-			{ display: <Icon icon="warning" className="yellow-text" /> },
-			{ display: 'Rainbow mix' },
-			{ display: 'Sale price is improperly formatted or the sale price provided is higher than the original price of...' },
-			{ display: <a href="#">Edit</a> },
-		],
-	];
+	const getRows = ( data ) => {
+		const statuses = {
+			success: 'green',
+			warning: 'yellow',
+			error: 'red',
+		}
+
+		const icons = {
+			success: 'yes-alt',
+			warning: 'warning',
+			error: 'warning'
+		}
+
+		return data.map( row => {
+			return (
+				[
+					{ display: <Icon icon={ icons[ row.status ] } className={ `${statuses[ row.status ]}-text` } /> },
+					{ display: row.product_name },
+					{ display: row.issue_description },
+					{ display: <a href={row.product_edit_link} target="_blank">{ __( 'Edit', 'pinterest-for-woocommerce' ) }</a> },
+				]
+			);
+		});
+	}
 
 	return (
 		<TableCard
 			className="pin4wc-catalog-sync__issues"
 			title={ __( 'Issues', 'pinterest-for-woocommerce' ) }
-		    rows={ rows }
+		    rows={ feedIssues && getRows( feedIssues?.lines ) }
 		    headers={ headers }
 			showMenu={ false }
 		    query={ { page: 1 } }
 		    rowsPerPage={ 5 }
 		    totalRows={ 10 }
+			isLoading={ ! feedIssues }
 		/>
 	)
 };
 
-export default SyncIssues;
+export default compose(
+	withSelect( ( select ) => {
+		const { getFeedIssues } = select( REPORTS_STORE_NAME );
+
+		return {
+			feedIssues: getFeedIssues()
+		}
+	})
+)(SyncIssues);
