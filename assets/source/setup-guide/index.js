299311ef7d6fe5064108438176d24305df211be1
Paulo Iankoski <me@wpdev.com.br>
2021-06-04 20:47:38 -0300

    Add a draft router function to handle different views

--- assets/source/setup-guide/index.js
+++ assets/source/setup-guide/index.js
@@ -1,16 +1,42 @@
 /**
  * External dependencies
  */
-import { render } from '@wordpress/element';
+import { render, useEffect, useState } from '@wordpress/element';
+import { getHistory, getQuery } from '@woocommerce/navigation';
 
 /**
  * Internal dependencies
  */
+import LandingPageApp from './app/views/LandingPageApp';
 import SettingsApp from './app/views/SettingsApp';
+import WizardApp from './app/views/WizardApp';
 import './app/style.scss';
+
+const App = () => {
+	const [ view, setView ] = useState( getQuery()?.view );
+
+	useEffect( () => {
+		const currentUrl = new URL( window.location.href );
+		const view = currentUrl.searchParams.get( 'view' );
+
+		setView( view );
+	});
+
+	getHistory().listen( () => {
+		setView( getQuery()?.view );
+	});
+
+	return view === 'settings' || getQuery()?.page === 'pinterest-for-woocommerce-setup-guide'
+		? <SettingsApp />
+		: view === 'wizard'
+			? <WizardApp />
+			: <LandingPageApp />;
+};
 
 const appRoot = document.getElementById( 'pin4wc-setup-guide' );
 
 if ( appRoot ) {
-	render( <SettingsApp />, appRoot );
+	render( <App />, appRoot );
 }
+
+export default App;
