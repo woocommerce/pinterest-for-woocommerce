dba84f70e6b0e306cb80a9cce411d372a1fd062c
Kostas Seresiotis <kostas@saucal.com>
2021-06-25 20:56:01 +0300

    always unschedule tasks

--- uninstall.php
+++ uninstall.php
@@ -17,9 +17,9 @@ $plugin_settings = get_option( 'pinterest_for_woocommerce' );
 if ( $plugin_settings['erase_plugin_data'] ) {
 	delete_option( 'pinterest_for_woocommerce' );
 	delete_option( 'pinterest_for_woocommerce_data' );
+}
 
-	if ( function_exists( 'as_unschedule_all_actions' ) ) {
-		as_unschedule_all_actions( 'pinterest-for-woocommerce-handle-sync', array(), 'pinterest-for-woocommerce' );
-		as_unschedule_all_actions( 'pinterest-for-woocommerce-feed-generation', array(), 'pinterest-for-woocommerce' );
-	}
+if ( function_exists( 'as_unschedule_all_actions' ) ) {
+	as_unschedule_all_actions( 'pinterest-for-woocommerce-handle-sync', array(), 'pinterest-for-woocommerce' );
+	as_unschedule_all_actions( 'pinterest-for-woocommerce-feed-generation', array(), 'pinterest-for-woocommerce' );
 }
