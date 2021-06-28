7febe454648a6ea8a01022730a50ff65b7a10f78
Fellyph Cintra <Cintra>
2021-06-29 00:00:15 +0100

    adding try again button at Select your advertiser step, bug related to issue #48

--- assets/source/setup-guide/app/steps/SetupTracking.js
+++ assets/source/setup-guide/app/steps/SetupTracking.js
@@ -349,7 +349,7 @@ const SetupTracking = ( { goToNextStep, view } ) => {
 											</>
 										)
 									) : (
-										<Spinner />
+										<StepButton />
 									) ) }
 							</CardBody>
 						) : (
