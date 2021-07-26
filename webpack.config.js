10fc13aea762ca412f3c7e2ca9c18a599b6d4a60
Kostas Seresiotis <kostas@saucal.com>
2021-07-26 15:23:41 +0300

    add deps to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,52 @@
 // Load the default @wordpress/scripts config object
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
+const path = require( 'path' );
+
+const requestToExternal = ( request ) => {
+	// The following default externals are bundled for compatibility with older versions of WP
+	// Note CSS for specific components is bundled via admin/assets/src/index.scss
+	// WP 5.4 is the min version for <Card* />, <TabPanel />
+	const bundled = [
+		'@wordpress/compose',
+		'@wordpress/components',
+		'@wordpress/primitives',
+	];
+	if ( bundled.includes( request ) ) {
+		return false;
+	}
+
+	const wcDepMap = {
+		'@woocommerce/components': [ 'wc', 'components' ],
+		'@woocommerce/navigation': [ 'wc', 'navigation' ],
+	};
+
+	return wcDepMap[ request ];
+};
+
+const requestToHandle = ( request ) => {
+	const wcHandleMap = {
+		'@woocommerce/components': 'wc-components',
+		'@woocommerce/navigation': 'wc-navigation',
+	};
+
+	return wcHandleMap[ request ];
+};
 
 // Use the defaultConfig but replace the entry and output properties
 const SetupGuide = {
     ...defaultConfig,
+    plugins: [
+		...defaultConfig.plugins.filter(
+			( plugin ) =>
+				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
+		),
+		new DependencyExtractionWebpackPlugin( {
+			injectPolyfill: true,
+			requestToExternal,
+			requestToHandle,
+		} ),
+	],
     entry: {
         index: './assets/source/setup-guide/index.js',
     },
@@ -15,6 +58,17 @@ const SetupGuide = {
 
 const SetupTask = {
     ...defaultConfig,
+    plugins: [
+		...defaultConfig.plugins.filter(
+			( plugin ) =>
+				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
+		),
+		new DependencyExtractionWebpackPlugin( {
+			injectPolyfill: true,
+			requestToExternal,
+			requestToHandle,
+		} ),
+	],
     entry: {
         index: './assets/source/setup-task/index.js',
     },
@@ -26,6 +80,17 @@ const SetupTask = {
 
 const CatalogSync = {
     ...defaultConfig,
+    plugins: [
+		...defaultConfig.plugins.filter(
+			( plugin ) =>
+				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
+		),
+		new DependencyExtractionWebpackPlugin( {
+			injectPolyfill: true,
+			requestToExternal,
+			requestToHandle,
+		} ),
+	],
     entry: {
         index: './assets/source/catalog-sync/index.js',
     },
