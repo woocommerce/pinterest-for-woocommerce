3cb21ca31723903560f25e6162cec0efd5b14a97
Matias Saggiorato <matiassaggiorato@gmail.com>
2021-08-11 23:16:25 -0300

    Rearrange code for easier maintenance.

--- assets/source/setup-guide/app/views/LandingPageApp.js
+++ assets/source/setup-guide/app/views/LandingPageApp.js
@@ -18,7 +18,77 @@ import {
  * Internal dependencies
  */
 
-const LandingPageApp = () => {
+const welcomeSection = () => {
+	return (
+		<Card className="woocommerce-table pinterest-for-woocommerce-landing-page__welcome-section">
+			<Flex>
+				<FlexBlock className="content-block">
+					<Text variant="title.medium">
+						{ __(
+							'Get your products in front of more than 475M people on Pinterest',
+							'pinterest-for-woocommerce'
+						) }
+					</Text>
+
+					<Text variant="body">
+						{ __(
+							'Pinterest is a visual discovery engine people use to find inspiration for their lives! More than 475 million people have saved more than 300 billion Pins, making it easier to turn inspiration into their next purchase.',
+							'pinterest-for-woocommerce'
+						) }
+					</Text>
+
+					<Text variant="body">
+						<Button
+							isPrimary
+							onClick={ () =>
+								getHistory().push(
+									getNewPath(
+										{},
+										wcSettings.pinterest_for_woocommerce
+											.isSetupComplete
+											? '/pinterest/catalog'
+											: '/pinterest/onboarding'
+									)
+								)
+							}
+						>
+							{ __( 'Get started', 'pinterest-for-woocommerce' ) }
+						</Button>
+					</Text>
+
+					<Text variant="body">
+						{ __(
+							'By clicking ‘Get started’, you agree to our',
+							'pinterest-for-woocommerce'
+						) }{ ' ' }
+						<a
+							href="https://business.pinterest.com/business-terms-of-service/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							{ __(
+								'Terms of Service',
+								'pinterest-for-woocommerce'
+							) }
+						</a>
+						.
+					</Text>
+				</FlexBlock>
+				<FlexBlock className="image-block">
+					<img
+						src={
+							wcSettings.pinterest_for_woocommerce.pluginUrl +
+							'/assets/images/landing_welcome.png'
+						}
+						alt=""
+					/>
+				</FlexBlock>
+			</Flex>
+		</Card>
+	);
+};
+
+const featuresSection = () => {
 	const features = [
 		{
 			title: __( 'Sync your catalog', 'pinterest-for-woocommerce' ),
@@ -55,6 +125,36 @@ const LandingPageApp = () => {
 		},
 	];
 
+	return (
+		<Card className="woocommerce-table pinterest-for-woocommerce-landing-page__features-section">
+			<Flex justify="center" align="top">
+				{ features.map( ( item, index ) => (
+					<FlexBlock key={ index }>
+						<img src={ item.image_url } alt="" />
+						<Text variant="subtitle">{ item.title }</Text>
+						<Text
+							variant="body"
+							dangerouslySetInnerHTML={ {
+								__html: item.text,
+							} }
+						/>
+						{ item?.extra && (
+							<Text
+								variant="body"
+								className="extra"
+								dangerouslySetInnerHTML={ {
+									__html: item.extra,
+								} }
+							/>
+						) }
+					</FlexBlock>
+				) ) }
+			</Flex>
+		</Card>
+	);
+};
+
+const faqSection = () => {
 	const faqItems = [
 		{
 			question: __(
@@ -80,120 +180,35 @@ const LandingPageApp = () => {
 	];
 
 	return (
+		<Card className="woocommerce-table pinterest-for-woocommerce-landing-page__faq-section">
+			<Panel
+				header={ __(
+					'Frequently asked questions',
+					'pinterest-for-woocommerce'
+				) }
+			>
+				{ faqItems.map( ( item, index ) => (
+					<PanelBody
+						title={ item.question }
+						initialOpen={ false }
+						key={ index }
+					>
+						<PanelRow>{ item.answer }</PanelRow>
+					</PanelBody>
+				) ) }
+			</Panel>
+		</Card>
+	);
+};
+
+const LandingPageApp = () => {
+	return (
 		<div className="pinterest-for-woocommerce-landing-page">
-			<Card className="woocommerce-table pinterest-for-woocommerce-landing-page__welcome-section">
-				<Flex>
-					<FlexBlock className="content-block">
-						<Text variant="title.medium">
-							{ __(
-								'Get your products in front of more than 475M people on Pinterest',
-								'pinterest-for-woocommerce'
-							) }
-						</Text>
+			{ welcomeSection() }
 
-						<Text variant="body">
-							{ __(
-								'Pinterest is a visual discovery engine people use to find inspiration for their lives! More than 475 million people have saved more than 300 billion Pins, making it easier to turn inspiration into their next purchase.',
-								'pinterest-for-woocommerce'
-							) }
-						</Text>
+			{ featuresSection() }
 
-						<Text variant="body">
-							<Button
-								isPrimary
-								onClick={ () =>
-									getHistory().push(
-										getNewPath(
-											{},
-											wcSettings.pinterest_for_woocommerce
-												.isSetupComplete
-												? '/pinterest/catalog'
-												: '/pinterest/onboarding'
-										)
-									)
-								}
-							>
-								{ __(
-									'Get started',
-									'pinterest-for-woocommerce'
-								) }
-							</Button>
-						</Text>
-
-						<Text variant="body">
-							{ __(
-								'By clicking ‘Get started’, you agree to our',
-								'pinterest-for-woocommerce'
-							) }{ ' ' }
-							<a
-								href="https://business.pinterest.com/business-terms-of-service/"
-								target="_blank"
-								rel="noreferrer"
-							>
-								{ __(
-									'Terms of Service',
-									'pinterest-for-woocommerce'
-								) }
-							</a>
-							.
-						</Text>
-					</FlexBlock>
-					<FlexBlock className="image-block">
-						<img
-							src={
-								wcSettings.pinterest_for_woocommerce.pluginUrl +
-								'/assets/images/landing_welcome.png'
-							}
-							alt=""
-						/>
-					</FlexBlock>
-				</Flex>
-			</Card>
-
-			<Card className="woocommerce-table pinterest-for-woocommerce-landing-page__features-section">
-				<Flex justify="center" align="top">
-					{ features.map( ( item, index ) => (
-						<FlexBlock key={ index }>
-							<img src={ item.image_url } alt="" />
-							<Text variant="subtitle">{ item.title }</Text>
-							<Text
-								variant="body"
-								dangerouslySetInnerHTML={ {
-									__html: item.text,
-								} }
-							/>
-							{ item?.extra && (
-								<Text
-									variant="body"
-									className="extra"
-									dangerouslySetInnerHTML={ {
-										__html: item.extra,
-									} }
-								/>
-							) }
-						</FlexBlock>
-					) ) }
-				</Flex>
-			</Card>
-
-			<Card className="woocommerce-table pinterest-for-woocommerce-landing-page__faq-section">
-				<Panel
-					header={ __(
-						'Frequently asked questions',
-						'pinterest-for-woocommerce'
-					) }
-				>
-					{ faqItems.map( ( item, index ) => (
-						<PanelBody
-							title={ item.question }
-							initialOpen={ false }
-							key={ index }
-						>
-							<PanelRow>{ item.answer }</PanelRow>
-						</PanelBody>
-					) ) }
-				</Panel>
-			</Card>
+			{ faqSection() }
 		</div>
 	);
 };
