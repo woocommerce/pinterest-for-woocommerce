1133d6f0ab0f2f305d132ff41ea9cb0068427fed
Jason Conroy <jason@findingsimple.com>
2021-10-06 21:33:01 +1030

    Version bump to 1.0.0-beta.3

--- class-pinterest-for-woocommerce.php
+++ class-pinterest-for-woocommerce.php
@@ -59,7 +59,7 @@ if ( ! class_exists( 'Pinterest_For_Woocommerce' ) ) :
 		 *
 		 * @var string
 		 */
-		public $version = '1.0.0-beta.2';
+		public $version = '1.0.0-beta.3';
 
 		/**
 		 * The single instance of the class.
