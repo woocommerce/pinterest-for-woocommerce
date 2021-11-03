745ccc0088fba53ae798491625c17b66c8713345
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-11-03 16:27:48 +0100

    Install JSDoc, with `jsdoc-plugin-typescript`,
    to run on our imported types.
    
    Related to
    - https://github.com/woocommerce/pinterest-for-woocommerce/issues/232
    - https://github.com/woocommerce/google-listings-and-ads/issues/117

--- assets/source/components/navigation-classic/index.js
+++ assets/source/components/navigation-classic/index.js
@@ -6,7 +6,7 @@ import MainTabNav from './main-tab-nav';
 /**
  * Display tab-based navigation when the new WC Navigation is not enabled.
  *
- * @return {import("./main-tab-nav")} Retuns MainTabNav if WC Navigation is not enabled.
+ * @return {import("./main-tab-nav").default} Retuns MainTabNav if WC Navigation is not enabled.
  */
 const NavigationClassic = () => {
 	const navigationEnabled = !! window.wcAdminFeatures?.navigation;
