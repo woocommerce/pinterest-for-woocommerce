c1f981c579d769abe8981df52ff9074e028c15c0
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2021-11-24 11:13:15 +0100

    Finish update procedure.

--- src/PluginUpdate.php
+++ src/PluginUpdate.php
@@ -36,8 +36,77 @@ class PluginUpdate {
 	 * @return void
 	 */
 	public static function update_to_2_0_0() {
+		/*
+		 * 1. Cancel old actions.
+		 */
 		as_unschedule_all_actions( PINTEREST_FOR_WOOCOMMERCE_PREFIX . '-handle-sync', array(), PINTEREST_FOR_WOOCOMMERCE_PREFIX );
 		as_unschedule_all_actions( PINTEREST_FOR_WOOCOMMERCE_PREFIX . '-feed-generation', array(), PINTEREST_FOR_WOOCOMMERCE_PREFIX );
-		$feed_id = Pinterest_For_Woocommerce()::get_data( 'local_feed_id' );
+
+		/*
+		 * 2. Move feed file to a new location.
+		 */
+		$feed_id          = Pinterest_For_Woocommerce()::get_data( 'local_feed_id' );
+		$default_location = Pinterest_For_Woocommerce()::get_base_country() ?? 'US';
+		if ( $feed_id ) {
+			// Generate new configurations.
+			$new_configs = ( new LocalFeedConfigs( $default_location ) )->get_configurations();
+			// We only migrate the default location, other configs do not exist at this stage.
+			$new_config = $new_configs[ $default_location ];
+
+			$upload_dir = wp_get_upload_dir();
+			$old_config = array(
+				'feed_file' => trailingslashit( $upload_dir['basedir'] ) . PINTEREST_FOR_WOOCOMMERCE_LOG_PREFIX . '-' . $feed_id . '.xml',
+				'tmp_file'  => trailingslashit( $upload_dir['basedir'] ) . PINTEREST_FOR_WOOCOMMERCE_LOG_PREFIX . '-' . $feed_id . '-tmp.xml',
+			);
+
+			if ( file_exists( $old_config['feed_file'] ) ) {
+				rename( $old_config['feed_file']['tmp_file'], $new_config['feed_file'] );
+			}
+			if ( file_exists( $old_config['tmp_file'] ) ) {
+				unlink( $old_config['feed_file'] );
+			}
+
+			/*
+			 * 2-a Next call to FeedRegistration::register_feed() will handle the url change. So we can skip this step and rely on automation.
+			 */
+
+			/*
+			 * 2-b. Move state.
+			 */
+			$data_prefix = PINTEREST_FOR_WOOCOMMERCE_PREFIX . '_feed_' . $feed_id . '_';
+
+			$old_state = array(
+				'status'        => get_transient( $data_prefix . 'status' ),
+				'current_index' => get_transient( $data_prefix . 'current_index' ),
+				'last_activity' => get_transient( $data_prefix . 'last_activity' ),
+				'product_count' => get_transient( $data_prefix . 'product_count' ),
+				'error_message' => get_transient( $data_prefix . 'error_message' ),
+			);
+
+			foreach ( $old_state as $key => $value ) {
+				delete_transient( $data_prefix . $key );
+			}
+
+			unset( $old_state['product_count'] );
+
+			ProductFeedStatus::set( $old_state );
+
+			/*
+			 * 2-c. PINTEREST_FOR_WOOCOMMERCE_PREFIX . '_feed_dataset_' transient will be removed deleted after WEEK_IN_SECONDS.
+			 */
+		}
+
+
+
+		/*
+		 * 3. Clear data.
+		 */
+		$settings = Pinterest_For_Woocommerce()::get_settings( true, PINTEREST_FOR_WOOCOMMERCE_DATA_NAME );
+
+		unset( $settings['local_feed_id'] );
+
+		Pinterest_For_Woocommerce()::save_settings( $settings, PINTEREST_FOR_WOOCOMMERCE_DATA_NAME );
+
+		// Update done.
 	}
 }
