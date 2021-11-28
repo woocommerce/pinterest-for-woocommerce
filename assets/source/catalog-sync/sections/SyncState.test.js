b3e42d2c684b852da63569b9752e0e63025e4675
Ian Yu-Hsun Lin <ianlin@automattic.com>
2021-11-28 11:23:22 +0800

    Remove an unnecessary test on catalog overview's footer
    Since we've already tested the text in the link, tests for the rest of
    the text can be safely removed.

--- assets/source/catalog-sync/sections/SyncState.test.js
+++ assets/source/catalog-sync/sections/SyncState.test.js
@@ -11,14 +11,9 @@ import SyncState from './SyncState';
 
 describe( 'SyncState component', () => {
 	test( 'should render header and footer correctly', () => {
-		const { getByText, getByRole } = render( <SyncState /> );
+		const { getByRole } = render( <SyncState /> );
 
 		expect( getByRole( 'heading' ) ).toHaveTextContent( 'Overview' );
-
-		expect(
-			getByText( 'Set up and manage ads to increase your reach with' )
-		).toBeTruthy();
-
 		expect( getByRole( 'link' ) ).toHaveTextContent(
 			'Pinterest ads manager'
 		);
