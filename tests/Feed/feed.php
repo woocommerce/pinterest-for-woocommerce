405802388dc6b26a34339e773985a036ac1712c2
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2022-01-06 11:12:27 +0100

    Add footer and header tests.

--- tests/Feed/feed.php
+++ tests/Feed/feed.php
@@ -1,9 +1,32 @@
 <?php
 
+namespace Automattic\WooCommerce\Pinterest;
+
 class Pinterest_Test_Feed extends \WC_Unit_Test_Case {
-	public function testFeed()
-	{
-		$this->assertTrue(true);
+	/**
+	 * @group feed
+	 */
+	public function testHeader() {
+		$actual_header = ProductsXmlFeed::get_xml_header();
+		$this->assertEquals(
+		"<?xml version=\"1.0\"?>
+<rss version=\"2.0\" xmlns:g=\"http://base.google.com/ns/1.0\">
+	<channel>
+",
+		$actual_header
+		);
+	}
+
+	/**
+	 * @group feed
+	 */
+	public function testFooter() {
+		$actual_footer = ProductsXmlFeed::get_xml_footer();
+		$this->assertEquals(
+		"	</channel>
+</rss>",
+		$actual_footer
+		);
 	}
 }
 
