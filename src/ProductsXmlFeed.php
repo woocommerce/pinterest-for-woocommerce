4a33154dcb07158bd71566e4caf0c014f43ea9ee
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2022-02-18 14:13:23 +0100

    Escape SKU for feed XML.

--- src/ProductsXmlFeed.php
+++ src/ProductsXmlFeed.php
@@ -395,7 +395,7 @@ class ProductsXmlFeed {
 	 * @return string
 	 */
 	private static function get_property_g_mpn( $product, $property ) {
-		return '<' . $property . '>' . $product->get_sku() . '</' . $property . '>';
+		return '<' . $property . '>' . esc_xml( $product->get_sku() ) . '</' . $property . '>';
 	}
 
 
