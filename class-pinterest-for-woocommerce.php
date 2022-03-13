c2acd8108c83bfef82d50b6c69464745892e653f
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2022-03-13 20:01:57 +0100

    Add update code entry point.

--- class-pinterest-for-woocommerce.php
+++ class-pinterest-for-woocommerce.php
@@ -235,7 +235,7 @@ if ( ! class_exists( 'Pinterest_For_Woocommerce' ) ) :
 			add_action( 'init', array( Pinterest\Tracking::class, 'maybe_init' ) );
 			add_action( 'init', array( Pinterest\ProductSync::class, 'maybe_init' ) );
 			add_action( 'init', array( Pinterest\TrackerSnapshot::class, 'maybe_init' ) );
-			add_action( 'plugins_loaded', array( Pinterest\PluginUpdate::class, 'maybe_update_plugin' ) );
+			add_action( 'plugins_loaded', array( $this, 'maybe_update_plugin' ) );
 
 			add_action( 'pinterest_for_woocommerce_token_saved', array( $this, 'set_default_settings' ) );
 			add_action( 'pinterest_for_woocommerce_token_saved', array( $this, 'update_account_data' ) );
@@ -331,6 +331,17 @@ if ( ! class_exists( 'Pinterest_For_Woocommerce' ) ) :
 			}
 
 			return false;
+		}
+
+		/**
+		 * Plugin update entry point.
+		 *
+		 * @since x.x.x
+		 * @return void
+		 */
+		public function maybe_update_plugin() {
+			$plugin_update = new Pinterest\PluginUpdate();
+			$plugin_update->maybe_update();
 		}
 
 		/**
