9bcba67a755d65071f098fdffef9532eddb35753
Alejandro Iglesias <alexiglesias3110@gmail.com>
2022-03-22 10:45:39 -0400

    Revert wrong added line on merge conflict resolution

--- class-pinterest-for-woocommerce.php
+++ class-pinterest-for-woocommerce.php
@@ -252,7 +252,6 @@ if ( ! class_exists( 'Pinterest_For_Woocommerce' ) ) :
 			// Disconnect advertiser if advertiser or tag change.
 			add_action( 'update_option_pinterest_for_woocommerce', array( $this, 'maybe_disconnect_advertiser' ), 10, 2 );
 
-			$this->maybe_update_plugin();
 		}
 
 
