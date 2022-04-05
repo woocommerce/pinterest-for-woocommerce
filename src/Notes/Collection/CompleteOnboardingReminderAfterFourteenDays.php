67d4e89ecd269eab8aa4afab7668649f83934207
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2022-04-05 12:27:00 +0200

    PHPCS CompleteOnboardingReminderAfterFourteenDays.php

--- src/Notes/Collection/CompleteOnboardingReminderAfterFourteenDays.php
+++ src/Notes/Collection/CompleteOnboardingReminderAfterFourteenDays.php
@@ -1,7 +1,22 @@
 <?php
+/**
+ * Pinterest for WooCommerce CompleteOnboardingAfterFourteenDays class.
+ *
+ * @package Pinterest_For_WooCommerce/Classes/
+ * @version x.x.x
+ */
 
 namespace Automattic\WooCommerce\Pinterest\Notes\Collection;
 
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class CompleteOnboardingAfterFourteenDays.
+ *
+ * Class responsible for admin Inbox notification after three days from setup.
+ *
+ * @since x.x.x
+ */
 class CompleteOnboardingReminderAfterFourteenDays extends CompleteOnboardingReminderAfterSevenDays {
 
 	const DELAY = 14;
