9f174f8e64bd44826f9d23c05f97e04747e40cb1
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2022-04-08 09:04:48 +0200

    Boolean -> bool
    Co-authored-by: Jeremy Pry <JPry@users.noreply.github.com>

--- src/Notes/Collection/EnableCatalogSync.php
+++ src/Notes/Collection/EnableCatalogSync.php
@@ -27,7 +27,7 @@ class EnableCatalogSync extends AbstractNote {
 	 * Should the note be added to the inbox.
 	 *
 	 * @since x.x.x
-	 * @return boolean
+	 * @return bool
 	 */
 	public function should_be_added(): bool {
 		if ( ! Pinterest_For_Woocommerce()::is_setup_complete() ) {
