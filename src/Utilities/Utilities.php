271127de2019ba40fa5b294c081f11bd0f32e768
Jeremy Pry <jeremy.pry@gmail.com>
2022-06-07 16:14:40 -0400

    Correct the docblocks and simplify the timestamp setting method

--- src/Utilities/Utilities.php
+++ src/Utilities/Utilities.php
@@ -23,22 +23,16 @@ class Utilities {
 	 * Set the account connection timestamp.
 	 *
 	 * @since x.x.x
-	 * @return int
 	 */
-	public static function set_account_connection_timestamp(): int {
-		$timestamp = time();
-
-		add_option( self::PINTEREST_ACCOUNT_CONNECTION_TIMESTAMP, $timestamp );
-
-		return $timestamp;
+	public static function set_account_connection_timestamp() {
+		add_option( self::PINTEREST_ACCOUNT_CONNECTION_TIMESTAMP, time() );
 	}
 
 	/**
 	 * Gets the account connection timestamp.
-	 * Initialize if not.
 	 *
 	 * @since x.x.x
-	 * @return int Initialization timestamp. Zero if not set.
+	 * @return int Account connection timestamp. Zero if not set.
 	 */
 	public static function get_account_connection_timestamp(): int {
 		return (int) get_option( self::PINTEREST_ACCOUNT_CONNECTION_TIMESTAMP, 0 );
