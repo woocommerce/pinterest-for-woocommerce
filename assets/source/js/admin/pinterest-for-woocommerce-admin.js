908625009748648dcebddbd3a03fd87f2e656d07
Alejandro Iglesias <alexiglesias3110@gmail.com>
2022-09-06 19:28:20 +0200

    force hiding tabs when page is loaded

--- assets/source/js/admin/pinterest-for-woocommerce-admin.js
+++ assets/source/js/admin/pinterest-for-woocommerce-admin.js
@@ -3,11 +3,15 @@
 ( function ( $ ) {
 	'use strict';
 
-	$( document ).on(
-		'woocommerce-product-type-change',
-		'body',
-		function ( event, productType ) {
-			$( '.hide_if_' + productType ).hide();
+	function hideTabs( event, productType ) {
+		$( '.hide_if_' + productType ).hide();
+	}
+
+	$( document ).on( 'woocommerce-product-type-change', 'body', hideTabs );
+
+	$( document ).ready( function () {
+		if ( $( '#product-type' ).length > 0 ) {
+			hideTabs( false, $( '#product-type' ).val() );
 		}
-	);
+	} );
 } )( jQuery );
