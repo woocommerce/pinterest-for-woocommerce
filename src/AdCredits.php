34a0d38cc3b8570547fd854e7ba679cb4cfc8ca5
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2022-09-27 13:42:05 +0200

    add comments and simplify.

--- src/AdCredits.php
+++ src/AdCredits.php
@@ -118,33 +118,44 @@ class AdCredits {
 		}
 	}
 
+	/**
+	 * Check if the ads campaign option is enabled in marketing recommendations.
+	 *
+	 * @since x.x.x
+	 *
+	 * @return void
+	 */
 	public static function check_if_ads_campaign_is_active() {
 
 		$is_campaign_active = get_transient( self::ADS_CREDIT_CAMPAIGN_TRANSIENT );
 
-		if ( true || false === $is_campaign_active ) {
-			$request         = wp_remote_get( 'https://woocommerce.com/wp-json/wccom/marketing-tab/1.1/recommendations.json' );
-			$recommendations = array();
-
-			if ( ! is_wp_error( $request ) && 200 === $request['response']['code'] ) {
-				$recommendations = json_decode( $request['body'], true );
-			}
-
-			foreach ( $recommendations as $recommendation ) {
-				if ( 'pinterest-for-woocommerce' === $recommendation['product'] ) {
-					$is_campaign_active = $recommendation['show_extension_promotions'] ?? false;
-					break;
-				}
-			}
-			$is_campaign_active = wc_bool_to_string( $is_campaign_active );
-			set_transient(
-				self::ADS_CREDIT_CAMPAIGN_TRANSIENT,
-				wc_bool_to_string( $is_campaign_active ),
-				empty( $recommendations ) ? 15 * MINUTE_IN_SECONDS : 12 * HOUR_IN_SECONDS
-			);
+		// If transient is available then it means that we have already checked.
+		if ( false !== $is_campaign_active ) {
+			return;
 		}
 
-		Pinterest_For_Woocommerce()->save_setting( self::ADS_CREDIT_CAMPAIGN_OPTION, wc_string_to_bool( $is_campaign_active ) );
+		$request         = wp_remote_get( 'https://woocommerce.com/wp-json/wccom/marketing-tab/1.2/recommendations.json' );
+		$recommendations = array();
+
+		if ( ! is_wp_error( $request ) && 200 === $request['response']['code'] ) {
+			$recommendations = json_decode( $request['body'], true );
+		}
+
+		// Find Pinterest plugin entry and check for promotions key.
+		foreach ( $recommendations as $recommendation ) {
+			if ( 'pinterest-for-woocommerce' === $recommendation['product'] ) {
+				$is_campaign_active = array_key_exists( 'show_extension_promotions', $recommendation ) ? $recommendation['show_extension_promotions'] : false;
+				break;
+			}
+		}
+
+		Pinterest_For_Woocommerce()->save_setting( self::ADS_CREDIT_CAMPAIGN_OPTION, $is_campaign_active );
+
+		set_transient(
+			self::ADS_CREDIT_CAMPAIGN_TRANSIENT,
+			wc_bool_to_string( $is_campaign_active ),
+			empty( $recommendations ) ? 15 * MINUTE_IN_SECONDS : 12 * HOUR_IN_SECONDS
+		);
 	}
 
 }
