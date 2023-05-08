03525b5a8eea91416aeed63c2c134b6810f9a3ea
Dima <dmytro.maksiuta@automattic.com>
2023-05-08 19:13:59 +0300

    Add filter to filter refresh access token start value.

--- src/RefreshToken.php
+++ src/RefreshToken.php
@@ -10,6 +10,7 @@ namespace Automattic\WooCommerce\Pinterest;
 
 use Exception;
 use Pinterest_For_Woocommerce;
+use WC_Log_Levels;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -43,26 +44,48 @@ class RefreshToken {
 		$expires_in   = intval( $token_data['expires_in'] );
 		$refresh_date = intval( $token_data['refresh_date'] );
 
-		if ( $refresh_date + $expires_in - 2 * DAY_IN_SECONDS <= time() ) {
+		/**
+		 * Filters to determine if it is time to start refreshing the token.
+		 *
+		 * @since x.x.x
+		 *
+		 * @param bool $maybe_refresh - True if it is time to refresh the token.
+		 */
+		$maybe_refresh = apply_filters(
+			PINTEREST_FOR_WOOCOMMERCE_OPTION_NAME . '_maybe_refresh_token',
+			$refresh_date + $expires_in - 2 * DAY_IN_SECONDS <= time()
+		);
+
+		// Translators: %s is the value of $maybe_refresh.
+		$refresh_log = sprintf( 'Should refresh pinterest access token: %s', wc_bool_to_string( $maybe_refresh ) );
+		Logger::log( $refresh_log, WC_Log_Levels::DEBUG, 'pinterest-for-woocommerce' );
+
+		if ( $maybe_refresh ) {
 			try {
 				$endpoint = Pinterest_For_Woocommerce::get_connection_proxy_url() . 'renew/' . PINTEREST_FOR_WOOCOMMERCE_WOO_CONNECT_SERVICE;
-				$response = wp_safe_remote_post(
-					$endpoint,
-					array(
-						'headers' => array(
-							'Content-Type' => 'application/x-www-form-urlencoded',
-						),
-						'body'    => array(
-							'refresh_token' => Crypto::decrypt( $token_data['refresh_token'] ),
-						),
-						'sslverify' => false,
-					)
+				$options  = array(
+					'headers' => array(
+						'Content-Type' => 'application/x-www-form-urlencoded',
+					),
+					'body'    => array(
+						'refresh_token' => Crypto::decrypt( $token_data['refresh_token'] ),
+					),
+					'sslverify' => false,
 				);
-				$body = trim( wp_remote_retrieve_body( $response ) );
+
+				// Translators: %1$s is the endpoint, %2$s is the options.
+				$request_log = sprintf( 'Refresh token request to %1$s with %2$s', $endpoint, json_encode( $options ) );
+				Logger::log( $request_log, WC_Log_Levels::DEBUG, 'pinterest-for-woocommerce' );
+
+				$response = wp_safe_remote_post( $endpoint, $options );
+				$body     = trim( wp_remote_retrieve_body( $response ) );
+
+				Logger::log( $body, WC_Log_Levels::DEBUG, 'pinterest-for-woocommerce' );
+
 				$body = json_decode( $body, true );
 				Pinterest_For_Woocommerce::save_token_data( $body );
 			} catch ( Exception $e ) {
-				// Log the error.
+				Logger::log( $e->getMessage(), WC_Log_Levels::ERROR, 'pinterest-for-woocommerce' );
 				return false;
 			}
 		}
