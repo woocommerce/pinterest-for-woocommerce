e0d6016c4ffc8d3cad7a29c0180c5f695d1d2ed5
Dima <dmytro.maksiuta@automattic.com>
2023-06-01 19:06:11 +0300

    Fix code styling.

--- src/API/AdvertiserConnect.php
+++ src/API/AdvertiserConnect.php
@@ -61,11 +61,13 @@ class AdvertiserConnect extends VendorAPI {
 				throw new Exception( esc_html__( 'Missing advertiser or tag parameters.', 'pinterest-for-woocommerce' ), 400 );
 			}
 
-			// Automatic Enhanced Match is enabled by default (at least Pinterest said so).
-			// if ( $enable_aem ) {
-				// @TODO: We do not have an API v5 analog for this call. Commenting it out temporarily.
-				// self::enable_aem_tag( $tag_id );
-			// }
+			/*
+			 * @NOTE: Automatic Enhanced Match is enabled by default (at least Pinterest said so).
+			 * if ( $enable_aem ) {
+			 *     @TODO: We do not have an API v5 analog for this call. Commenting it out temporarily.
+			 *     self::enable_aem_tag( $tag_id );
+			 * }
+			 */
 
 			$is_connected = Pinterest_For_Woocommerce()::get_data( 'is_advertiser_connected' );
 
