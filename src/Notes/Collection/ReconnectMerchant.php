a0d3796351e58e237899406b16b57a2caf03e783
Dima <dmytro.maksiuta@automattic.com>
2023-06-14 21:58:13 +0300

    Add exception handling. Fix php code styling error.

--- src/Notes/Collection/ReconnectMerchant.php
+++ src/Notes/Collection/ReconnectMerchant.php
@@ -10,6 +10,7 @@ namespace Automattic\WooCommerce\Pinterest\Notes\Collection;
 use Automattic\WooCommerce\Admin\Notes\Note;
 use Automattic\WooCommerce\Pinterest\UnauthorizedAccessMonitor;
 use Exception;
+use Throwable;
 
 /**
  * Class ReconnectMerchant.
@@ -39,11 +40,16 @@ class ReconnectMerchant extends AbstractNote {
 	 *
 	 * @since x.x.x
 	 *
-	 * @throws Exception
+	 * @throws Exception If note is not found.
 	 * @return bool
 	 */
 	public static function should_be_added(): bool {
-		return UnauthorizedAccessMonitor::is_as_task_paused() && ! self::note_exists();
+		$is_task_paused = UnauthorizedAccessMonitor::is_as_task_paused();
+		try {
+			return $is_task_paused && ! self::note_exists();
+		} catch ( Throwable $e ) {
+			return true;
+		}
 	}
 
 	/**
