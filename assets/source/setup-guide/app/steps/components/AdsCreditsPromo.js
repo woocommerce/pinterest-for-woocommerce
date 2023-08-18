ef1e96cf4a0686033b1d7f2af6dc9cbdff7d3cf8
Kruti Dugade <krutidugade@Krutis-MacBook-Pro.local>
2023-08-19 00:57:20 +0100

    Dynamic generation of credits given and spend requirement amount on Ads promo and onboarding modal.

--- assets/source/setup-guide/app/steps/components/AdsCreditsPromo.js
+++ assets/source/setup-guide/app/steps/components/AdsCreditsPromo.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { useState, createInterpolateElement } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { recordEvent } from '@woocommerce/tracks';
 import {
 	CardDivider,
@@ -42,6 +42,8 @@ const AdsCreditsPromo = () => {
 		} );
 	};
 
+	const currencyCreditInfo = appSettings?.account_data?.currency_credit_info;
+
 	return appSettings?.ads_campaign_is_active ? (
 		<>
 			<CardDivider
@@ -54,9 +56,14 @@ const AdsCreditsPromo = () => {
 				<FlexBlock className="content-block">
 					<Text variant="body">
 						{ createInterpolateElement(
-							__(
-								'As a new Pinterest customer, you can get $125 in free ad credits when you successfully set up Pinterest for WooCommerce and spend $15 on Pinterest Ads. <a>Pinterest Terms and conditions</a> apply.',
-								'pinterest-for-woocommerce'
+							sprintf(
+								//  translators: %1$s: Amount of ad credits given with currency. %2$s: Amount of money required to spend to claim ad credits with currency.
+								__(
+									'As a new Pinterest customer, you can get %1$s in free ad credits when you successfully set up Pinterest for WooCommerce and spend %2$s on Pinterest Ads. <a>Pinterest Terms and conditions</a> apply.',
+									'pinterest-for-woocommerce'
+								),
+								currencyCreditInfo.creditsGiven,
+								currencyCreditInfo.spendRequire
 							),
 							{
 								a: (
