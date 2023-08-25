b25bfcfeb4780ad20738dee5b8fbf231f2e922a6
Kruti Dugade <krutidugade@Krutis-MacBook-Pro.local>
2023-08-25 14:38:45 +0100

    Updates minimum PHP to 7.4 and PHP 8.2 for coding standards workflow.

--- class-pinterest-for-woocommerce.php
+++ class-pinterest-for-woocommerce.php
@@ -61,7 +61,7 @@ if ( ! class_exists( 'Pinterest_For_Woocommerce' ) ) :
 		 * Set the minimum required versions for the plugin.
 		 */
 		const PLUGIN_REQUIREMENTS = array(
-			'php_version'      => '7.3',
+			'php_version'      => '7.4',
 			'wp_version'       => '5.6',
 			'wc_version'       => '5.3',
 			'action_scheduler' => '3.3.0',
--- pinterest-for-woocommerce.php
+++ pinterest-for-woocommerce.php
@@ -23,7 +23,7 @@
  *
  * Requires at least: 5.6
  * Tested up to: 6.3
- * Requires PHP: 7.3
+ * Requires PHP: 7.4
  *
  * WC requires at least: 5.3
  * WC tested up to: 8.0
