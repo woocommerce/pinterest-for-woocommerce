4b3f6292d5a4c803f00aae254a87d4d9f02cfe8c
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2023-11-06 11:54:55 +0100

    Remove response parse.

--- src/API/TokenExchangeV3ToV5.php
+++ src/API/TokenExchangeV3ToV5.php
@@ -22,27 +22,6 @@ class TokenExchangeV3ToV5 extends APIV5 {
 	const API_DOMAIN = 'https://api.pinterest.com/v3';
 
 	/**
-	 * Return array with response data.
-	 * We need this funcion becase all ther API endpoints return request data in body but this returns in data.
-	 *
-	 * @since x.x.x
-	 *
-	 * @param mixed $response The response to parse.
-	 *
-	 * @return array
-	 *
-	 * @throws Exception PHP exception.
-	 */
-	protected static function parse_response( $response ) {
-
-		if ( ! array_key_exists( 'body', (array) $response ) ) {
-			throw new Exception( __( 'Empty data response', 'pinterest-for-woocommerce' ), 204 );
-		}
-
-		return json_decode( $response['data'], true );
-	}
-
-	/**
 	 * Pull ads supported countries information from the API.
 	 *
 	 * @since x.x.x
