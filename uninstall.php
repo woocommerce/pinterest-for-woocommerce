76a8d22d15f7bc12bcdddbf0ade273b9c7d5476f
Dima <9010963+message-dimke@users.noreply.github.com>
2024-02-13 15:59:47 +0200

    Fixing phpcs errors.

--- uninstall.php
+++ uninstall.php
@@ -16,8 +16,7 @@ require_once __DIR__ . '/pinterest-for-woocommerce.php';
 
 try {
 	Pinterest_For_Woocommerce::disconnect();
-} catch ( Throwable $th ) {
-	// phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedCatch
+} catch ( Throwable $th ) { // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedCatch
 	// Do nothing - this is a cleanup routine.
 }
 
