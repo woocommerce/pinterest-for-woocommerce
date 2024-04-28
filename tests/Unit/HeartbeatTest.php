4741ca18ade9519528e8e685b2122bb57dfa9656
Dima <9010963+message-dimke@users.noreply.github.com>
2024-04-28 20:46:41 +0300

    Updating the test.

--- tests/Unit/HeartbeatTest.php
+++ tests/Unit/HeartbeatTest.php
@@ -3,6 +3,7 @@
 namespace Automattic\WooCommerce\Pinterest\Tests\Unit;
 
 use Automattic\WooCommerce\Pinterest\Heartbeat;
+use Pinterest_For_Woocommerce;
 
 class HeartbeatTest extends \WP_UnitTestCase {
 
@@ -20,6 +21,9 @@ class HeartbeatTest extends \WP_UnitTestCase {
 	 * @return void
 	 */
 	public function test_cancel_jobs_removes_daily_and_hourly_as_actions() {
+		// Jobs will schedule only if Pinterest is connected (means integration data is set and has the ID).
+		Pinterest_For_Woocommerce::save_data( 'integration_data', array( 'id' => '567891567892' ) );
+
 		$this->heartbeat->schedule_events();
 
 		$this->assertTrue( as_has_scheduled_action( Heartbeat::HOURLY, array(), 'pinterest-for-woocommerce' ) );
