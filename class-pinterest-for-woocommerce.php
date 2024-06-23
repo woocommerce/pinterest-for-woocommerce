32386b5be3b1ed64edd59c8c49969c43ab60a7b1
Dima <9010963+message-dimke@users.noreply.github.com>
2024-06-23 14:39:35 +0300

    Introducing new `track_conversions_capi` setting to store CAPI tracker status. Forcing it to false not to enable the tracker per Pinterest requirement. Adding unit tests to cover trackers initialization logic (CAPI can only be initialized if Tag is also ON, no CAPI tracker alone is possible).

--- class-pinterest-for-woocommerce.php
+++ class-pinterest-for-woocommerce.php
@@ -125,6 +125,7 @@ if ( ! class_exists( 'Pinterest_For_Woocommerce' ) ) :
 		 */
 		protected static $default_settings = array(
 			'track_conversions'                => true,
+			'track_conversions_capi'           => false,
 			'enhanced_match_support'           => true,
 			'automatic_enhanced_match_support' => true,
 			'save_to_pinterest'                => true,
@@ -327,17 +328,28 @@ if ( ! class_exists( 'Pinterest_For_Woocommerce' ) ) :
 			 *
 			 * @param bool $disable_tracking Whether to disable tracking.
 			 */
-			$is_tracking_disabled             = apply_filters( 'woocommerce_pinterest_disable_tracking', false );
-			$is_tracking_conversions_disabled = ! Pinterest_For_Woocommerce()::get_setting( 'track_conversions' );
-			$is_not_a_site                    = wp_doing_cron() || is_admin();
-
-			if ( $is_tracking_disabled || $is_tracking_conversions_disabled || $is_not_a_site ) {
+			$is_tracking_disabled = apply_filters( 'woocommerce_pinterest_disable_tracking', false );
+			$is_not_a_site        = wp_doing_cron() || is_admin();
+			if ( $is_tracking_disabled || $is_not_a_site ) {
 				return false;
 			}
 
-			$tag_tracker = new Tag();
+			$is_tracking_conversions_enabled      = Pinterest_For_Woocommerce()::get_setting( 'track_conversions' );
+			$is_tracking_conversions_capi_enabled = Pinterest_For_Woocommerce()::get_setting( 'track_conversions_capi' );
 
-			return new Tracking( array( $tag_tracker ) );
+			if ( ! $is_tracking_conversions_enabled ) {
+				return false;
+			}
+
+			$tracking = new Tracking( array( new Tag() ) );
+
+			if ( $is_tracking_conversions_capi_enabled ) {
+				$user                = new User( WC_Geolocation::get_ip_address(), wc_get_user_agent() );
+				$conversions_tracker = new Conversions( $user );
+				$tracking->add_tracker( $conversions_tracker );
+			}
+
+			return $tracking;
 		}
 
 		/**
--- tests/Unit/TrackingTest.php
+++ tests/Unit/TrackingTest.php
@@ -13,19 +13,6 @@ class TrackingTest extends \WP_UnitTestCase {
 		parent::setUp();
 	}
 
-	public function test_init_tracking_inits() {
-		Pinterest_For_Woocommerce::save_settings( array( 'track_conversions' => true ) );
-		add_filter( 'wp_doing_cron', '__return_false' );
-
-		$tracking = Pinterest_For_Woocommerce::init_tracking();
-
-		$this->assertEquals( 10, has_action( 'wp_footer', array( $tracking, 'handle_page_visit' ) ) );
-		$this->assertEquals( 10, has_action( 'wp_footer', array( $tracking, 'handle_view_category' ) ) );
-		$this->assertEquals( 10, has_action( 'woocommerce_add_to_cart', array( $tracking, 'handle_add_to_cart' ) ) );
-		$this->assertEquals( 10, has_action( 'woocommerce_before_thankyou', array( $tracking, 'handle_checkout' ) ) );
-		$this->assertEquals( 10, has_action( 'wp_footer', array( $tracking, 'handle_search' ) ) );
-	}
-
 	function test_tracking_adds_actions_monitoring() {
 		$tracking = new Tracking();
 
