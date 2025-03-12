c22a5bf10179d443c3062b4cd6747e21d46862ea
Dima <9010963+message-dimke@users.noreply.github.com>
2025-03-12 12:45:44 +0200

    Adding a comment about the change.

--- src/LocaleMapper.php
+++ src/LocaleMapper.php
@@ -120,6 +120,7 @@ class LocaleMapper {
 	 * @return string
 	 */
 	private static function get_wordpress_locale() {
+		// Using `WPLANG` instead `get_locale()` since the last may return inconsistent results depending on the user session language.
 		$wordpress_locale = get_option( 'WPLANG', self::PINTEREST_DEFAULT_LOCALE );
 		return str_replace( '_', '-', $wordpress_locale );
 	}
