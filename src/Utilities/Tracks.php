832a10130e8ab9ed194331b743e47f66fdda1068
Justin Palmer <228780+layoutd@users.noreply.github.com>
2026-02-26 12:15:45 +0100

    Remove WC 6.x compatibility code

--- src/Utilities/Tracks.php
+++ src/Utilities/Tracks.php
@@ -24,11 +24,6 @@ trait Tracks {
 	 * @param array  $properties Array of properties to include with the event.
 	 */
 	private static function record_event( string $name, array $properties = array() ): void {
-		if ( class_exists( WC_Tracks::class ) ) {
-			WC_Tracks::record_event( $name, $properties );
-		} elseif ( function_exists( 'wc_admin_record_tracks_event' ) ) {
-			wc_admin_record_tracks_event( $name, $properties );
-		}
+		WC_Tracks::record_event( $name, $properties );
 	}
-
 }
